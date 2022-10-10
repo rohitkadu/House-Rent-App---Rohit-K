b9280a5b2c016f85acdf3b941155fa7b3b4d7458
Rohit Kadu <rohitkadu3102@gmail.com>
2022-10-10 19:46:06 +0530

    Google Sign In and Sign Out Complete using Firebase

--- app/src/main/java/com/example/houserentappproject/activity_login_user.java
+++ app/src/main/java/com/example/houserentappproject/activity_login_user.java
@@ -1,12 +1,27 @@
 package com.example.houserentappproject;
 
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 
 public class activity_login_user extends AppCompatActivity {
+
+    GoogleSignInClient mGoogleSignInClient ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,14 +29,69 @@ public class activity_login_user extends AppCompatActivity {
         setContentView(R.layout.activity_login_user);
 
 
-//        Intent act_user_reg = new Intent(activity_login_user.this , activity_registration_user.class);
-//
-//        new Handler().postDelayed(new Runnable() {
-//            @Override
-//            public void run() {
-//                startActivity(act_user_reg);
-//            }
-//        },3000);
+        ImageView img_google_sign_in = findViewById(R.id.img_google_sign_in);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        img_google_sign_in.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switch (view.getId()) {
+                    case R.id.img_google_sign_in:
+                        signIn();
+                        break;
+                    // ...
+                }
+            }
+        });
+
+    }
+
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, 100);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 100) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> task) {
+
+        try {
+            GoogleSignInAccount account = task.getResult(ApiException.class);
+            
+            OnLoggedIn(account);
+            
+            // Signed in successfully, show authenticated UI.
+//            finish();
+//            Intent successful_google_signin = new Intent(activity_login_user.this, activity_successful_google_signin.class);
+//            startActivity(successful_google_signin);
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
+        }
+
+    }
+
+    private void OnLoggedIn(GoogleSignInAccount googleSignInAccount) {
+
+        Intent intent = new Intent(this, activity_successful_google_signin.class);
+        intent.putExtra(activity_successful_google_signin.GOOGLE_ACCOUNT, googleSignInAccount);
+
+        startActivity(intent);
+        finish();
 
     }
 }
--- app/src/main/java/com/example/houserentappproject/activity_successful_google_signin.java
+++ app/src/main/java/com/example/houserentappproject/activity_successful_google_signin.java
@@ -1,14 +1,97 @@
 package com.example.houserentappproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.squareup.picasso.Picasso;
+
+import org.w3c.dom.Text;
 
 public class activity_successful_google_signin extends AppCompatActivity {
+
+    public static final String GOOGLE_ACCOUNT = "google_account";
+    ImageView profile_image ;
+    TextView profile_name , profile_email ;
+    Button btn_sign_out , btn_continue ;
+    GoogleSignInClient mGoogleSignInClient ;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_successful_google_signin);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        GoogleSignInAccount googleSignInAccount = getIntent().getParcelableExtra(GOOGLE_ACCOUNT);
+
+        profile_image = findViewById(R.id.profile_image);
+        profile_name = findViewById(R.id.profile_name);
+        profile_email = findViewById(R.id.profile_email);
+        btn_sign_out = findViewById(R.id.btn_sign_out);
+        btn_continue = findViewById(R.id.btn_continue);
+
+
+//        Picasso.get().load(googleSignInAccount.getPhotoUrl()).into(profile_image);
+        profile_name.setText(googleSignInAccount.getDisplayName());
+        profile_email.setText(googleSignInAccount.getEmail());
+
+
+
+
+
+        btn_sign_out.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switch (view.getId()) {
+                    case R.id.btn_sign_out:
+                        signOut();
+                        break;
+                }
+            }
+        });
+
+//        mGoogleSignInClient.signOut()
+//                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<Void> task) {
+//                        Intent signin_again = new Intent(activity_successful_google_signin.this , activity_login_user.class);
+//                        startActivity(signin_again);
+//                    }
+//                });
+
+
+    }
+
+    private void signOut() {
+                mGoogleSignInClient.signOut()
+                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        Intent signin_again = new Intent(activity_successful_google_signin.this , activity_login_user.class);
+                        startActivity(signin_again);
+                        finish();
+                    }
+                });
     }
 }
